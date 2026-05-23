d53bb55c73a187458f31c7eee7d4ea9b306b2907
jonpalencia <jonpalencia20@gmail.com>
2026-05-23 23:55:44 +0800

    Add: switch and active functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
+import { useState } from 'react';
 import Player from './components/Player';
 import GameBoard from './components/GameBoard';
 
 function App() {
+  const [activePlayer, setactivePlayer] = useState('X');
+  const onSelectSquare = function () {
+    setactivePlayer(prevSelect => (prevSelect === 'X' ? 'O' : 'X'));
+  };
+
   return (
     <menu>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player 1" symbol="X" />
-          <Player initialName="Player 2" symbol="O" />
+        <ol className="highlight-player" id="players">
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === 'X'}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === 'O'}
+          />
         </ol>
-        <GameBoard />
+        <GameBoard
+          selectHandler={onSelectSquare}
+          activePlayerSymbol={activePlayer}
+        />
       </div>
     </menu>
   );
