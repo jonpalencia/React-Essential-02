76cbb831228a8238c8f39ec223e31dbee4d4d4a2
jonpalencia <jonpalencia20@gmail.com>
2026-05-24 04:57:12 +0800

    Add: new Log.jsx component

---
+++ src/components/Log.jsx
@@ -0,0 +1,11 @@
+export default function Log({ turns }) {
+  return (
+    <ol id="log">
+      {turns.map((turn, turnIndex) => (
+        <li key={`${turn.square.row}${turn.square.col}${turnIndex}`}>
+          {turn.player} selected {turn.square.row}, {turn.square.col}
+        </li>
+      ))}
+    </ol>
+  );
+}
