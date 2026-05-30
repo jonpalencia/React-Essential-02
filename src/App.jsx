32f4654f349effbd6d4630e44a873ed765e69ea6
jonpalencia <jonpalencia20@gmail.com>
2026-05-31 05:29:15 +0800

    refactor: fix App component with to use the config and helpers js file

--- src/App.jsx
+++ src/App.jsx
@@ -3,50 +3,24 @@ import Player from './components/Player';
 import GameBoard from './components/GameBoard';
 import Log from './components/Log';
 import GameOver from './components/GameOver';
-import { WINNING_COMBINATIONS } from './winning_combination';
-
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-const deriveStatePlayer = function (prevTurn) {
-  let updatedPlayer = 'X';
-  if (prevTurn.length > 0 && prevTurn[0].player === 'X') updatedPlayer = 'O';
-  return updatedPlayer;
-};
+import { INITIAL_GAMEBOARD, PLAYER_NAME } from './config/config';
+import { deriveStatePlayer, deriveHasWinner } from './helpers/helpers';
+import { WINNING_COMBINATIONS } from './config/winning_combination';
 
 export default function App() {
-  const [playerName, setPlayerName] = useState({
-    X: 'Player 1',
-    O: 'Player 2',
-  });
+  const [playerName, setPlayerName] = useState(PLAYER_NAME);
   const [gameTurns, setGameTurns] = useState([]);
   const activePlayer = deriveStatePlayer(gameTurns);
-
-  // More modern way of cloning or creating a deep copy of a nested array or object. Using the structuredClone(object) method
-  let gameBoard = [...initialGameBoard.map(arr => [...arr])];
+  const gameBoard = [...INITIAL_GAMEBOARD.map(arr => [...arr])];
 
   gameTurns.forEach(turn => {
     const {square: {row, col}, player} = turn; // prettier-ignore
     gameBoard[row][col] = player;
   });
 
-  let winner;
-  WINNING_COMBINATIONS.forEach(combination => {
-    const firstSquareCombination = gameBoard[combination[0].row][combination[0].column] // prettier-ignore
-    const secondSquareCombination = gameBoard[combination[1].row][combination[1].column] // prettier-ignore
-    const thirdSquareCombination = gameBoard[combination[2].row][combination[2].column] // prettier-ignore
-
-    if (
-      firstSquareCombination &&
-      firstSquareCombination === secondSquareCombination &&
-      firstSquareCombination === thirdSquareCombination
-    )
-      winner = playerName[firstSquareCombination].toUpperCase();
-  });
+  const winner = deriveHasWinner(WINNING_COMBINATIONS, gameBoard, playerName);
   const hasDraw = gameTurns.length === 9 && !winner;
+
   const handleRematch = function () {
     setGameTurns([]);
   };
@@ -79,13 +53,13 @@ export default function App() {
         <ol className="highlight-player" id="players">
           <Player
             onChangeName={handleNameChange}
-            initialName="Player 1"
+            initialName={PLAYER_NAME.X}
             symbol="X"
             isActive={activePlayer === 'X'}
           />
           <Player
             onChangeName={handleNameChange}
-            initialName="Player 2"
+            initialName={PLAYER_NAME.O}
             symbol="O"
             isActive={activePlayer === 'O'}
           />
